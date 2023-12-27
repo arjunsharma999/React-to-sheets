8a746cf36173d1d4c62c03e6cb30def19a29af45
Arjun Sharma <120898184+arjunsharma999@users.noreply.github.com>
2023-12-27 16:08:12 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,229 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import Swal from "sweetalert2";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [option1, setoprtion1] = useState("");
+  const [option2, setoption2] = useState("");
+  const [option3, setoprtion3] = useState("");
+  const [option4, setoprtion4] = useState("");
+  const [option5, setoprtion5] = useState("");
+  const [option6, setoprtion6] = useState("");
+  const [option7, setoprtion7] = useState("");
+  const [option8, setoprtion8] = useState("");
+  const [loading, setloading] = useState(true);
+
+
+
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+
+    try {
+
+      setloading(false)
+      const response = await fetch(
+        "https://v1.nocodeapi.com/arjun999/google_sheets/WMUoLCyVpMscFqLl?tabId=Sheet1",
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify([
+            [
+              email,
+              option1 ? "Yes" : "No",
+              option2 ? "Yes" : "No",
+              option3 ? "Yes" : "No",
+              option4 ? "Yes" : "No",
+              option5 ? "Yes" : "No",
+              option6 ? "Yes" : "No",
+              option7 ? "Yes" : "No",
+              option8 ? "Yes" : "No",
+              new Date().toLocaleString(), // Adjust date formatting if needed
+            ],
+          ]),
+        }
+      );
+
+      const allOption = [option1,
+        option2,
+        option3,
+        option4,
+        option5,
+        option6,
+        option7,
+        option8]
+
+      const countYes = allOption.filter(option => option != "").length;
+      const countNo = allOption.filter(option => option === "No").length;
+
+
+      if (countYes >= 7) {
+        Swal.fire({
+          title: 'Hurray, thanks for your response! ',
+          text: 'You are a Blended Champ! Keep Up the Good Work in 2024. Take Every Chance to Encourage Your Teammates to Participate in the Blended Behaviours.Click to Avail Your Gift..',
+          icon: 'success', 
+          confirmButtonText: 'Okay',
+        });
+      }
+      else if (countYes > 4) {
+        Swal.fire({
+          title: 'Hurray, thanks for your response! ',
+          text: 'You are a Blended Proficient! You are Doing Good with Scope for Improvement in 2024. So, Take Some Resolutions & Become a Blended Expert with the Right Behaviours.Click to Avail Your Gift.',
+          icon: 'success', 
+          confirmButtonText: 'Okay',
+        });
+      } else if (countYes <= 4) {
+        Swal.fire({
+          title: 'Hurray, thanks for your response! ',
+          text: 'You are a Blended Newbie! Commit to the Blended Behaviours in 2024 & Take Your Work Experience to the Next Level with our Tenets. Click to Avail Your Gift.',
+          icon: 'success', 
+          confirmButtonText: 'Okay',
+        });
+
+      }
+
+
+      setloading(true); // Assuming you meant to set loading back to false here
+      setEmail("");
+
+    } catch (error) {
+      console.error("Error submitting data:", error);
+      setloading(false);
+    }
+  };
+
+
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+      <div className="bg-gray-100 flex items-center justify-center h-screen">
+        <form
+        action="/"
+          id="myForm"
+          onSubmit={handleSubmit}
+          className="bg-white p-8 rounded shadow-md w-full sm:w-80"
+        >
+          <label className="block mb-4">
+            <input
+              type="checkbox"
+              value={"A"}
+              name="option1"
+              className="mr-2"
+              onChange={(e) => {
+                setoprtion1(e.target.value);
+              }}
             />
-          </a>
-        </div>
+            Option 1
+          </label>
+          <label className="block mb-4">
+            <input
+              type="checkbox"
+              value={"B"}
+              name="option2"
+              onChange={(e) => {
+                setoption2(e.target.value);
+              }}
+              className="mr-2"
+            />
+            Option 2
+          </label>
+          <label className="block mb-4">
+            <input
+              type="checkbox"
+              value={"C"}
+              name="option3"
+              onChange={(e) => {
+                setoprtion3(e.target.value);
+              }}
+              className="mr-2"
+            />
+            Option 3
+          </label>
+          <label className="block mb-4">
+            <input
+              type="checkbox"
+              value={"D"}
+              name="option4"
+              onChange={(e) => {
+                setoprtion4(e.target.value);
+              }}
+              className="mr-2"
+            />
+            Option 4
+          </label>
+          <label className="block mb-4">
+            <input
+              type="checkbox"
+              value={"E"}
+              name="option5"
+              onChange={(e) => {
+                setoprtion5(e.target.value);
+              }}
+              className="mr-2"
+            />
+            Option 5
+          </label>
+          <label className="block mb-4">
+            <input
+              type="checkbox"
+              value={"C"}
+              name="option6"
+              onChange={(e) => {
+                setoprtion6(e.target.value);
+              }}
+              className="mr-2"
+            />
+            Option 3
+          </label>
+          <label className="block mb-4">
+            <input
+              type="checkbox"
+              value={"D"}
+              name="option7"
+              onChange={(e) => {
+                setoprtion7(e.target.value);
+              }}
+              className="mr-2"
+            />
+            Option 4
+          </label>
+          <label className="block mb-4">
+            <input
+              type="checkbox"
+              value={"E"}
+              name="option8"
+              onChange={(e) => {
+                setoprtion8(e.target.value);
+              }}
+              className="mr-2"
+            />
+            Option 5
+          </label>
+          <label className="block mb-4">
+            <span className="block mb-1">Email:</span>
+            <input
+              type="email"
+              name="email"
+              className="w-full p-2 border border-gray-300 rounded"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              required
+            />
+          </label>
+          <input
+            type="submit"
+            value={loading ? "Submit " : "Loading..."}
+            onSubmit={handleSubmit}
+            className="bg-green-500 text-white p-2 w-full rounded cursor-pointer"
+          />
+        </form>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
